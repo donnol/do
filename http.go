5e28e473eb6192bcc3f533ad400921308669c6d7
jiandong.liu <jdlau@qq.com>
2023-03-31 11:10:01 +0800

    add send http request

---
+++ http.go
@@ -0,0 +1,103 @@
+package do
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type (
+	CodeChecker func(code int) error
+)
+
+func CodeIs200(code int) error {
+	if code != http.StatusOK {
+		return fmt.Errorf("bad http code: %d", code)
+	}
+	return nil
+}
+
+type (
+	ResultExtractor[R any] func(data []byte) (R, error)
+)
+
+func JSONExtractor[R any](data []byte) (R, error) {
+	var r R
+	if err := json.Unmarshal(data, &r); err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func XMLExtractor[R any](data []byte) (R, error) {
+	var r R
+	if err := xml.Unmarshal(data, &r); err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func SendHTTPRequest[R any](
+	client *http.Client,
+	method string,
+	link string,
+	body io.Reader,
+	header http.Header,
+	codeChecker CodeChecker,
+	extractResult ResultExtractor[R],
+) (R, error) {
+	var r R
+
+	if method == "" || link == "" {
+		return r, fmt.Errorf("bad param: method or link is empty")
+	}
+
+	req, err := http.NewRequest(method, link, body)
+	if err != nil {
+		return r, err
+	}
+	for k, v := range header {
+		for _, vv := range v {
+			req.Header.Set(k, vv)
+		}
+	}
+
+	if client == nil {
+		client = &http.Client{
+			Timeout: 10 * time.Second,
+		}
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return r, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return r, err
+	}
+
+	if codeChecker == nil {
+		codeChecker = CodeIs200
+	}
+	err = codeChecker(resp.StatusCode)
+	if err != nil {
+		return r, fmt.Errorf("check code failed: %v, data: %s", err, data)
+	}
+
+	if extractResult == nil {
+		extractResult = JSONExtractor[R]
+	}
+	r, err = extractResult(data)
+	if err != nil {
+		return r, fmt.Errorf("extract result failed: %v, data: %s", err, data)
+	}
+
+	return r, nil
+}
---
+++ http_test.go
@@ -0,0 +1,89 @@
+package do
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+type (
+	result struct {
+		BgQuality int `json:"BgQuality"`
+	}
+)
+
+func TestDoHTTPRequest(t *testing.T) {
+	type args[R any] struct {
+		client        *http.Client
+		method        string
+		link          string
+		body          io.Reader
+		header        http.Header
+		codeChecker   CodeChecker
+		extractResult ResultExtractor[result]
+	}
+	type testCase[R any] struct {
+		name    string
+		args    args[R]
+		want    result
+		wantErr bool
+	}
+	tests := []testCase[result]{
+		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args[result]{
+				method:        http.MethodGet,
+				link:          "https://www.bing.com/hp/api/model",
+				body:          nil,
+				codeChecker:   CodeIs200,
+				extractResult: JSONExtractor[result],
+			},
+			want: result{BgQuality: 50},
+		},
+		{
+			name: "nil",
+			args: args[result]{
+				method:        http.MethodGet,
+				link:          "https://www.bing.com/hp/api/model",
+				body:          nil,
+				codeChecker:   nil,
+				extractResult: nil,
+			},
+			want: result{BgQuality: 50},
+		},
+		{
+			name: "header",
+			args: args[result]{
+				method: http.MethodGet,
+				link:   "https://www.bing.com/hp/api/model",
+				body:   nil,
+				header: http.Header{
+					"User-Agent": []string{`Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36`},
+				},
+				codeChecker: func(code int) error {
+					if code != http.StatusOK {
+						return fmt.Errorf("code is %d", code)
+					}
+					return nil
+				},
+				extractResult: JSONExtractor[result],
+			},
+			want: result{BgQuality: 50},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SendHTTPRequest(tt.args.client, tt.args.method, tt.args.link, tt.args.body, tt.args.header, tt.args.codeChecker, tt.args.extractResult)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DoHTTPRequest() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DoHTTPRequest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
