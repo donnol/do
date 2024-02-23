cd90ac69f6841c7f41e0fa6f2510ba8a52489e67
donnol <donnol@outlook.com>
2024-02-23 16:06:15 +0800

    feat(math): add Factorial and FactorialBig

--- math.go
+++ math.go
@@ -1,6 +1,9 @@
 package do
 
-import "math"
+import (
+	"math"
+	"math/big"
+)
 
 func Round(x float64, p int) float64 {
 	if p == 0 {
@@ -33,4 +36,26 @@ func Ceil(x float64, p int) float64 {
 		n *= 10
 	}
 	return math.Ceil(x*n) / n
+}
+
+// Factorial n <= 20, it will return 0 if n > 20, use FactorialBig instead.
+func Factorial(n int) int {
+	if n > 20 {
+		return 0
+	}
+
+	s := 1
+	for i := 2; i <= n; i++ {
+		s *= i
+	}
+	return s
+}
+
+// FactorialBig n > 20
+func FactorialBig(n int) string {
+	s := big.NewInt(1)
+	for i := 2; i <= n; i++ {
+		s = s.Mul(s, big.NewInt(int64(i)))
+	}
+	return s.String()
 }
