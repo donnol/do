25c8c2b7324218f5e3f1d9aa844e9ceacf4a09b8
donnol <donnol@outlook.com>
2024-05-07 13:46:36 +0800

    feat(http): add HTTPSProxy

--- http_proxy_test.go
+++ http_proxy_test.go
@@ -1,8 +1,15 @@
 package do
 
 import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"io"
+	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 	"testing"
 	"time"
@@ -75,12 +82,18 @@ func startServer(addr string, ret []byte) {
 }
 
 func TestHTTPSProxy(t *testing.T) {
+	t.Skip()
+
+	// 为了避免错误：http server doesn't support hijacking connection
+	// From: https://stackoverflow.com/questions/67770829/go-http-request-falls-back-to-http2-even-when-force-attempt-is-set-to-false
+	os.Setenv("GODEBUG", "http2client=0")
+
 	type args struct {
 		localAddr  string
 		remoteAddr string
 		msg        []byte
 		cert, key  string
-		opt        *HTTPProxyOption
+		opt        *HTTPSProxyOption
 	}
 	tests := []struct {
 		name    string
@@ -96,7 +109,12 @@ func TestHTTPSProxy(t *testing.T) {
 				msg:        []byte("hello" + strconv.Itoa(rand.Int())),
 				cert:       "./testdata/cert/server.crt",
 				key:        "./testdata/cert/server.key",
-				opt:        &HTTPProxyOption{},
+				opt: &HTTPSProxyOption{
+					CertFile:   "./testdata/cert2/server.crt",
+					KeyFile:    "./testdata/cert2/server.key",
+					CaCertFile: "./testdata/cert3/server.crt",
+					CaKeyFile:  "./testdata/cert3/server.key",
+				},
 			},
 			wantErr: false,
 		},
@@ -106,10 +124,7 @@ func TestHTTPSProxy(t *testing.T) {
 			go startHTTPSServer(tt.args.remoteAddr, tt.args.msg, tt.args.cert, tt.args.key)
 
 			go func() {
-				// TODO:
-				// tt.args.opt.Cert = tt.args.cert
-				// tt.args.opt.Key = tt.args.key
-				if err := HTTPProxy(tt.args.localAddr, httpsPrefix+tt.args.remoteAddr, tt.args.opt); (err != nil) != tt.wantErr {
+				if err := HTTPSProxy(tt.args.localAddr, tt.args.opt); (err != nil) != tt.wantErr {
 					t.Errorf("HTTPProxy() error = %v, wantErr %v", err, tt.wantErr)
 				}
 			}()
@@ -117,7 +132,11 @@ func TestHTTPSProxy(t *testing.T) {
 
 			{
 				client := NewHTTPClient(HTTPClientSkipVerify())
-				r, err := SendHTTPRequest(client, http.MethodGet, httpsPrefix+tt.args.localAddr, nil, nil, CodeIs200, RawExtractor)
+				// From https://github.com/golang/go/issues/22554
+				// Go's HTTP client does not support sending a request with
+				// the CONNECT method. See the documentation on Transport for
+				// details.
+				r, err := SendHTTPRequest(client, http.MethodConnect, httpsPrefix+tt.args.localAddr+"/", nil, nil, CodeIs200, RawExtractor)
 				if err != nil {
 					t.Error(err)
 				}
@@ -139,5 +158,76 @@ func startHTTPSServer(addr string, ret []byte, cert, key string) {
 		Addr:    addr,
 		Handler: mux,
 	}
+	log.Println("Starting https server on", addr)
 	Must(s.ListenAndServeTLS(cert, key))
+}
+
+func TestHTTPSProxyByBing(t *testing.T) {
+	addr := "localhost:55689"
+	go func() {
+		if err := HTTPSProxy(addr, &HTTPSProxyOption{
+			CertFile:   "./testdata/cert2/server.crt",
+			KeyFile:    "./testdata/cert2/server.key",
+			CaCertFile: "./testdata/cert3/server.crt",
+			CaKeyFile:  "./testdata/cert3/server.key",
+		}); err != nil {
+			t.Errorf("HTTPProxy() error = %v", err)
+		}
+	}()
+	time.Sleep(time.Millisecond * 200)
+
+	r := Must1(http.NewRequest(http.MethodGet, "https://www.bing.com/hp/api/model", nil))
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			Proxy: func(r *http.Request) (*url.URL, error) {
+				return url.Parse("https://" + addr)
+			},
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	resp := Must1(client.Do(r))
+	defer resp.Body.Close()
+
+	data := Must1(io.ReadAll(resp.Body))
+	var res result
+	Must(json.Unmarshal(data, &res))
+	Assert(t, res.BgQuality, 50)
+}
+
+func TestHTTPSProxyByBaidu(t *testing.T) {
+	addr := "localhost:55690"
+	go func() {
+		if err := HTTPSProxy(addr, &HTTPSProxyOption{
+			CertFile:   "./testdata/cert2/server.crt",
+			KeyFile:    "./testdata/cert2/server.key",
+			CaCertFile: "./testdata/cert3/server.crt",
+			CaKeyFile:  "./testdata/cert3/server.key",
+		}); err != nil {
+			t.Errorf("HTTPProxy() error = %v", err)
+		}
+	}()
+	time.Sleep(time.Millisecond * 200)
+
+	r := Must1(http.NewRequest(http.MethodGet, "https://www.baidu.com", nil))
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			Proxy: func(r *http.Request) (*url.URL, error) {
+				return url.Parse("https://" + addr)
+			},
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	resp := Must1(client.Do(r))
+	defer resp.Body.Close()
+
+	data := Must1(io.ReadAll(resp.Body))
+	Assert(t, bytes.Contains(data, []byte("关于")), true, "resp.body is %s", data)
 }
