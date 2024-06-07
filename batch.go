9a78a45f99af9fe80e8c199d5545006d8f0c90e5
donnol <donnol@outlook.com>
2024-06-07 16:35:21 +0800

    feat(batch): add BatchRunR and StreamRunR

--- batch.go
+++ batch.go
@@ -27,6 +27,40 @@ func BatchRun[T any](s []T, batchNum int, handler func([]T) error) (err error) {
 	return
 }
 
+// BatchRunR handle data per batch
+func BatchRunR[T, R any](s []T, batchNum int, handler func([]T) ([]R, error)) (all []R, err error) {
+	all = make([]R, 0, len(s))
+
+	batch := make([]T, 0, batchNum)
+	for _, item := range s {
+		item := item
+		batch = append(batch, item)
+
+		if len(batch) < batchNum {
+			continue
+		}
+
+		// 达到一批了则执行
+		tv, err := handler(batch)
+		if err != nil {
+			return nil, err
+		}
+		all = append(all, tv...)
+
+		batch = make([]T, 0, batchNum)
+	}
+	// 剩余未满一批的
+	if len(batch) > 0 {
+		tv, err := handler(batch)
+		if err != nil {
+			return nil, err
+		}
+		all = append(all, tv...)
+	}
+
+	return
+}
+
 // StreamRun handle data by stream, if batchNum is >0, run with batch
 func StreamRun[T any](s chan T, batchNum int, handler func([]T) error) (err error) {
 	batch := make([]T, 0, batchNum)
@@ -51,6 +85,42 @@ func StreamRun[T any](s chan T, batchNum int, handler func([]T) error) (err erro
 		if err = handler(batch); err != nil {
 			return err
 		}
+	}
+
+	return
+}
+
+// StreamRunR handle data by stream, if batchNum is >0, run with batch
+func StreamRunR[T, R any](s chan T, batchNum int, handler func([]T) ([]R, error)) (all []R, err error) {
+	all = make([]R, 0, len(s))
+
+	batch := make([]T, 0, batchNum)
+	for e := range s {
+		e := e
+		if batchNum > 0 {
+			batch = append(batch, e)
+			if len(batch) < batchNum {
+				continue
+			}
+		} else {
+			batch = []T{e}
+		}
+
+		tv, err := handler(batch)
+		if err != nil {
+			return nil, err
+		}
+		all = append(all, tv...)
+
+		batch = make([]T, 0, batchNum)
+	}
+	// 剩余未满一批的
+	if len(batch) > 0 {
+		tv, err := handler(batch)
+		if err != nil {
+			return nil, err
+		}
+		all = append(all, tv...)
 	}
 
 	return
--- batch_test.go
+++ batch_test.go
@@ -2,6 +2,7 @@ package do
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -38,6 +39,70 @@ func TestBatchRun(t *testing.T) {
 			if err := BatchRun(tt.args.s, tt.args.batchNum, tt.args.handler); (err != nil) != tt.wantErr {
 				t.Errorf("BatchRun() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestBatchRunR(t *testing.T) {
+	type args struct {
+		s        []int
+		batchNum int
+		handler  func([]int) ([]string, error)
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    []string
+	}{
+		{
+			name: "1",
+			args: args{
+				s:        []int{1, 2, 3},
+				batchNum: 1,
+				handler: func(part []int) ([]string, error) {
+					if len(part) == 0 {
+						return nil, fmt.Errorf("len is 0")
+					}
+					// fmt.Println(part)
+					r := make([]string, 0, len(part))
+					for _, item := range part {
+						r = append(r, strconv.Itoa(item+1))
+					}
+					return r, nil
+				},
+			},
+			wantErr: false,
+			want:    []string{"2", "3", "4"},
+		},
+		{
+			name: "2",
+			args: args{
+				s:        []int{1, 2, 3},
+				batchNum: 2,
+				handler: func(part []int) ([]string, error) {
+					if len(part) == 0 {
+						return nil, fmt.Errorf("len is 0")
+					}
+					// fmt.Println(part)
+					r := make([]string, 0, len(part))
+					for _, item := range part {
+						r = append(r, strconv.Itoa(item+1))
+					}
+					return r, nil
+				},
+			},
+			wantErr: false,
+			want:    []string{"2", "3", "4"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BatchRunR(tt.args.s, tt.args.batchNum, tt.args.handler)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BatchRun() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			AssertSlice(t, got, tt.want)
 		})
 	}
 }
@@ -96,6 +161,77 @@ func TestStreamRun(t *testing.T) {
 			if err := StreamRun(tt.args.s, tt.args.batchNum, tt.args.handler); (err != nil) != tt.wantErr {
 				t.Errorf("StreamRun() error = %v, wantErr %v", err, tt.wantErr)
 			}
+		})
+	}
+}
+
+func TestStreamRunR(t *testing.T) {
+	type args struct {
+		s        chan int
+		batchNum int
+		handler  func([]int) ([]string, error)
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    []string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				s:        make(chan int, 5),
+				batchNum: 1,
+				handler: func(part []int) ([]string, error) {
+					if len(part) == 0 {
+						return nil, fmt.Errorf("len is 0")
+					}
+					// fmt.Println(s)
+					r := make([]string, 0, len(part))
+					for _, item := range part {
+						r = append(r, strconv.Itoa(item+1))
+					}
+					return r, nil
+				},
+			},
+			wantErr: false,
+			want:    []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
+		},
+		{
+			name: "2",
+			args: args{
+				s:        make(chan int, 5),
+				batchNum: 2,
+				handler: func(part []int) ([]string, error) {
+					if len(part) == 0 {
+						return nil, fmt.Errorf("len is 0")
+					}
+					// fmt.Println(s)
+					r := make([]string, 0, len(part))
+					for _, item := range part {
+						r = append(r, strconv.Itoa(item+1))
+					}
+					return r, nil
+				},
+			},
+			wantErr: false,
+			want:    []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			go func() {
+				for i := 0; i < 10; i++ {
+					tt.args.s <- i
+				}
+				close(tt.args.s)
+			}()
+			got, err := StreamRunR(tt.args.s, tt.args.batchNum, tt.args.handler)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StreamRun() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			AssertSlice(t, got, tt.want)
 		})
 	}
 }
